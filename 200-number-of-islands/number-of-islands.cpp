bc7bdc3a9d258c90463300affd83fe73cf9de648
Li Ge <geli19880206@gmail.com>
2016-09-01 00:08:23 -0700

    New solution script

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+ public:
+
+};
+
+int main(int argc, char* argv[]) {
+  Solution sol;
+  cout << "Hello world\n";
+  return 0;
+}
