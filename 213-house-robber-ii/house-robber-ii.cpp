4aff3ed5cf2107f5b57a1f9907a16bc840c45319
Li Ge <geli19880206@gmail.com>
2016-09-07 09:35:18 -0700

    [213-house-robber-ii] AC

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -14,22 +14,30 @@ using namespace std;
 
 class Solution {
  public:
-  // F(x) = max(F(x-2) + nums[x], F(x-1))
-  // Want: F(nums.size() - 1)
+  int robNoCircle(vector<int>& nums, size_t from, size_t to) {
+    if (to < from) return 0;
+    size_t n = to - from + 1;
+    if (n == 1) return nums[from];
+    if (n == 2) return max(nums[from], nums[from + 1]);
+
+    int a = nums[from], b = max(a, nums[from + 1]);
+    int c = 0;
+    for (size_t i = 2; i < n; ++i) {
+      c = max(a + nums[from + i], b);
+      a = b;
+      b = c;
+    }
+    return c;
+  }
+
   int rob(vector<int>& nums) {
     size_t n = nums.size();
     if (n == 0) return 0;
     if (n == 1) return nums[0];
     if (n == 2) return max(nums[0], nums[1]);
-
-    int a = nums[0], b = max(a, nums[1]);
-    int c = 0;
-    for (size_t i = 2; i < n; ++i) {
-      c = max(a + nums[i], b);
-      a = b;
-      b = c;
-    }
-    return c;
+    int a = robNoCircle(nums, 0, n - 2);
+    int b = robNoCircle(nums, 1, n - 3);
+    return max(a, b + nums[n - 1]);
   }
 };
 
@@ -52,7 +60,7 @@ int main(int argc, char* argv[]) {
     test(nums);
   }
   {
-    vector<int> nums = { 6, 4, 8, 4, 3 };
+    vector<int> nums = { 6, 6, 4, 8, 4, 3, 3, 10 };
     test(nums);
   }
   return 0;
