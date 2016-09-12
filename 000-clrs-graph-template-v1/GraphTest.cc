cc689a4782f29aadad498ea0bcc073c5842f8d08
Li Ge <geli19880206@gmail.com>
2016-09-12 12:19:24 -0700

    [000-clrs-graph-template-v1] Add tests for directed graph

---
+++ 000-clrs-graph-template-v1/GraphTest.cc
@@ -0,0 +1,104 @@
+#define CATCH_CONFIG_MAIN
+#include "000-external/catch.hpp"
+
+#include "Graph.hh"
+#include "GraphBuilder.hh"
+#include "GraphTextIO.hh"
+
+TEST_CASE( "Directed graph", "[DirectedGraph]" ) {
+  DirectedGraph<> g = GraphBuilder<DirectedGraph<> >::clrs_22_4();
+
+  SECTION("isDirected()") {
+    REQUIRE(g.isDirected() == true);
+  }
+
+  SECTION("hasVertex()") {
+    REQUIRE(g.hasVertex("u") == true);
+    REQUIRE(g.hasVertex("v") == true);
+    REQUIRE(g.hasVertex("w") == true);
+    REQUIRE(g.hasVertex("x") == true);
+    REQUIRE(g.hasVertex("y") == true);
+    REQUIRE(g.hasVertex("z") == true);
+    REQUIRE(g.hasVertex("a") == false);
+    REQUIRE(g.hasVertex("b") == false);
+  }
+
+  SECTION("numVertices()") {
+    REQUIRE(g.numVertices() == 6);
+  }
+
+  SECTION("getVertexState() && getMutableVertexState()") {
+    g.getMutableVertexState("u") = "u state 123";
+    REQUIRE(g.getVertexState("u") == "u state 123");
+    REQUIRE(g.getVertexState("v") == "");
+  }
+
+  SECTION("getEdgeState() && getMutableEdgeState()") {
+    g.getMutableEdgeState("u", "v") = "u -> v state 123";
+    REQUIRE(g.getEdgeState("u", "v") == "u -> v state 123");
+    REQUIRE(g.getEdgeState("v", "y") == "");
+  }
+
+  SECTION("numEdges()") {
+    REQUIRE(g.numEdges() == 8);
+  }
+
+  SECTION("hasEdge()") {
+    REQUIRE(g.hasEdge("u", "v") == true);
+    REQUIRE(g.hasEdge("v", "y") == true);
+    REQUIRE(g.hasEdge("u", "x") == true);
+    REQUIRE(g.hasEdge("x", "v") == true);
+    REQUIRE(g.hasEdge("y", "x") == true);
+    REQUIRE(g.hasEdge("w", "y") == true);
+    REQUIRE(g.hasEdge("w", "z") == true);
+    REQUIRE(g.hasEdge("z", "z") == true);
+
+    REQUIRE(g.hasEdge("v", "u") == false);
+    REQUIRE(g.hasEdge("v", "w") == false);
+    REQUIRE(g.hasEdge("x", "y") == false);
+  }
+
+  SECTION("all vertex iterator") {
+    std::set<std::string> expectedVertices = {
+      "u", "v", "w", "x", "y", "z"
+    };
+    std::set<std::string> actualVertices(g.vertexBegin(), g.vertexEnd());
+    REQUIRE(expectedVertices == actualVertices);
+  }
+
+  SECTION("neighbors vertex iterator of u") {
+    std::set<std::string> expectedNeighbors = { "v", "x" };
+    std::set<std::string> actualNeighbors(g.adjBegin("u"), g.adjEnd("u"));
+    REQUIRE(expectedNeighbors == actualNeighbors);
+  }
+
+  SECTION("neighbors vertex iterator of v") {
+    std::set<std::string> expectedNeighbors = { "y" };
+    std::set<std::string> actualNeighbors(g.adjBegin("v"), g.adjEnd("v"));
+    REQUIRE(expectedNeighbors == actualNeighbors);
+  }
+
+  SECTION("neighbors vertex iterator of w") {
+    std::set<std::string> expectedNeighbors = { "y", "z" };
+    std::set<std::string> actualNeighbors(g.adjBegin("w"), g.adjEnd("w"));
+    REQUIRE(expectedNeighbors == actualNeighbors);
+  }
+
+  SECTION("neighbors vertex iterator of x") {
+    std::set<std::string> expectedNeighbors = { "v" };
+    std::set<std::string> actualNeighbors(g.adjBegin("x"), g.adjEnd("x"));
+    REQUIRE(expectedNeighbors == actualNeighbors);
+  }
+
+  SECTION("neighbors vertex iterator of y") {
+    std::set<std::string> expectedNeighbors = { "x" };
+    std::set<std::string> actualNeighbors(g.adjBegin("y"), g.adjEnd("y"));
+    REQUIRE(expectedNeighbors == actualNeighbors);
+  }
+
+  SECTION("neighbors vertex iterator of z") {
+    std::set<std::string> expectedNeighbors = { "z" };
+    std::set<std::string> actualNeighbors(g.adjBegin("z"), g.adjEnd("z"));
+    REQUIRE(expectedNeighbors == actualNeighbors);
+  }
+}
