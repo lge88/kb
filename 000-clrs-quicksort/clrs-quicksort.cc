0b351f087a4f0e03165fd1e7977ec5f53afaa5db
Li Ge <geli19880206@gmail.com>
2016-11-22 16:46:54 -0800

    [000-clrs-quicksort] iterative quick sort

--- 000-clrs-quicksort/clrs-quicksort.cc
+++ 000-clrs-quicksort/clrs-quicksort.cc
@@ -4,38 +4,58 @@
 
 using namespace std;
 
-int partition(vector<int>& arr, int start, int end, int p) {
-  swap(arr[p], arr[end]);
-  int i = start - 1, x = arr[end];
-  for (int j = start; j <= end; ++j) {
-    if (arr[j] <= x) swap(arr[++i], arr[j]);
+int partition(vector<int>& arr, int start, int end) {
+  int q = start - 1, x = arr[end];
+  for (int i = start; i <= end; ++i) {
+    if (arr[i] <= x) swap(arr[++q], arr[i]);
   }
-  return i;
+  return q;
 }
 
 void quickSortRecursive(vector<int>& arr, int start, int end) {
   if (start >= end) return;
-  int p = start + rand() % (end - start + 1);
-  int q = partition(arr, start, end, p);
+  // swap(arr[start + rand() % (end - start + 1)], arr[end]);
+  int q = partition(arr, start, end);
   quickSortRecursive(arr, start, q - 1);
   quickSortRecursive(arr, q + 1, end);
 }
 
 void quickSortRecursive2(vector<int>& arr, int start, int end) {
   if (start >= end) return;
-
   // swap(arr[start + rand() % (end - start + 1)], arr[end]);
   int q = start - 1, x = arr[end];
   for (int i = start; i <= end; ++i) {
     if (arr[i] <= x) swap(arr[++q], arr[i]);
   }
-
   quickSortRecursive2(arr, start, q - 1);
   quickSortRecursive2(arr, q + 1, end);
 }
 
+struct Range {
+  int start_, end_;
+  Range(int start, int end): start_(start), end_(end) {}
+};
+
+void quickSortIterative(vector<int>& arr) {
+  int n = arr.size();
+  if (n <= 1) return;
+
+  vector<Range> stk;
+  stk.push_back(Range(0, n - 1));
+  while (!stk.empty()) {
+    int start = stk.back().start_, end = stk.back().end_;
+    stk.pop_back();
+    // swap(arr[start + rand() % (end - start + 1)], arr[end]);
+    int q = partition(arr, start, end);
+    if (start < q - 1) stk.push_back(Range(start, q - 1));
+    if (q + 1 < end) stk.push_back(Range(q + 1, end));
+  }
+}
+
 void quickSort(vector<int>& arr) {
-  quickSortRecursive2(arr, 0, arr.size() - 1);
+  // quickSortRecursive(arr, 0, arr.size() - 1);
+  // quickSortRecursive2(arr, 0, arr.size() - 1);
+  quickSortIterative(arr);
 }
 
 int main(int argc, char* argv[]) {
